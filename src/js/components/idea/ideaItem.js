885022e0646e7e5d2c681a1efa13fee7f0942583
Felicia Ringblom <felicia.ringblom@gmail.com>
2016-01-05 13:54:19 +0100

    added list of ideas

--- src/js/components/idea/ideaItem.js
+++ src/js/components/idea/ideaItem.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-export default () => {
-  return (
-    <div clasName="idea-container">
-      <h3 className="idea-heading">Idea heading</h3>
-      <p className="idea-desc">Lorem ipsum...</p>
-      <p className="idea-added-date">2015-10-10</p>
-      <button type="button">Like</button>
-      <button type="button">Dislike</button>
-      <ul className="idea-tag-list">
-        <li>Tech</li>
-        <li>App</li>
-      </ul>
-    </div>
-  );
+class IdeaItem extends React.Component {
+  render() {
+    return (
+      <div clasName="idea-container">
+        <h3 className="idea-heading">{this.props.heading}</h3>
+        <p className="idea-desc">{this.props.desc}</p>
+        <p className="idea-added-date">{this.props.added}</p>
+        <button type="button">Like</button>
+        <button type="button">Dislike</button>
+        <ul className="idea-tag-list">
+          <li>Tech</li>
+          <li>App</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+IdeaItem.propTypes = {
+  heading: React.PropTypes.string.isRequired,
+  desc: React.PropTypes.string.isRequired,
+  added: React.PropTypes.string.isRequired,
 };
+
+export default IdeaItem;
